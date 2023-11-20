bb2ccd7421626a71c0d3510f534431992d290614
Andebugulin <gulin.andrey2005@gmail.com>
2023-11-20 14:18:39 +0200

    exercise2.6 is done

---
+++ part2/exercise_2.6-2.10/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+const Header = ({ text }) => {
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
+};
+
+const PhonebookForm = ({ persons, setPersons, newName, setNewName }) => {
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log("adding a person \n name:", newName);
+
+    console.log("new person has", newName, "name", "id:", persons.length);
+    setPersons([...persons, { id: persons.length, name: newName }]);
+    setNewName("");
+  };
+
+  const handlePersonChange = (event) => {
+    console.log("typing into a person form, \nsomething:", event.target.value);
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={addPerson}>
+        name:
+        <input value={newName} onChange={handlePersonChange} />
+        <button type="submit">add</button>
+      </form>
+    </div>
+  );
+};
+
+const NumbersRender = ({ person }) => {
+  return <li>{person.name}</li>;
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([{ id: 0, name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("Dartanyan");
+
+  return (
+    <div>
+      <Header text="Phonebook" />
+      <PhonebookForm
+        persons={persons}
+        setPersons={setPersons}
+        newName={newName}
+        setNewName={setNewName}
+      />
+      <Header text="Numbers" />
+      <ul>
+        {persons.map((person) => (
+          <NumbersRender key={person.id} person={person} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;

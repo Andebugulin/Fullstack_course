e9a30cd66985120121052609989c3c1c6b6e57e4
Andebugulin <gulin.andrey2005@gmail.com>
2023-12-02 11:04:15 +0200

    On branch main
     message = exercise 2.18 is done
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.jsx

--- part2/exercise_2.18-2.20/src/App.jsx
+++ part2/exercise_2.18-2.20/src/App.jsx
@@ -1,35 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const DisplayCountryData = ({ countryData }) => {
+  console.log(countryData);
+  const officialName = countryData.name.official;
+  const capitalName = countryData.capital;
+  const area = countryData.area;
+  const languages = countryData.languages;
+  const flagImageHref = countryData.flags.png;
+  const flagAlt = countryData.flags.alt;
+  console.log("languages", languages);
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <h1>{officialName}</h1>
+        <p>capital: {capitalName}</p>
+        <p>area: {area}</p>
+        <h2>languages</h2>
+        <ul>
+          {Object.entries(languages).map(([key, value]) => (
+            <li key={key}>{value}</li>
+          ))}
+        </ul>
 
-export default App
+        <img alt={flagAlt} src={flagImageHref} />
+      </div>
+    </div>
+  );
+};
+
+/**
+ *
+ * @param {Array} country
+ * @returns
+ */
+const CountryDisplay = ({ country }) => {
+  console.log("displaying country");
+  if (country.length >= 10) {
+    return <div>Too many matches, specify another filter!</div>;
+  } else if (country.length >= 2) {
+    return (
+      <ol>
+        {country.map((countryName, index) => (
+          <li key={index}>{countryName.name.official}</li>
+        ))}
+      </ol>
+    );
+  } else if (country.length === 1) {
+    return <DisplayCountryData countryData={country[0]} />;
+  } else if (country.length === 0) {
+    return <div>No matches found.</div>;
+  }
+};
+
+const Filter = ({ filter, setFilter, setCountry, countries }) => {
+  const handleFilterChange = (event) => {
+    event.preventDefault();
+    const newFilter = event.target.value.toLowerCase();
+    setFilter(newFilter);
+    console.log("newFilter:", newFilter);
+
+    const filteredCountries = countries.filter((country) =>
+      country.name.official.toLowerCase().includes(newFilter.toLowerCase())
+    );
+    setCountry(filteredCountries);
+    console.log("filteredCountries", filteredCountries);
+  };
+
+  return (
+    <div>
+      <form>
+        find country: <input value={filter} onChange={handleFilterChange} />
+      </form>
+    </div>
+  );
+};
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    console.log("effect run, currency is now", country);
+    console.log("fetching exchange rates...");
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then((response) => {
+        setCountries(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Filter
+        filter={filter}
+        setFilter={setFilter}
+        setCountry={setCountry}
+        countries={countries}
+      />
+      <CountryDisplay country={country} />
+    </div>
+  );
+};
+
+export default App;
